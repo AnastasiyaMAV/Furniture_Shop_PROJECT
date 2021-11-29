94663bed88a2e7d2357e057168c33ee71710237f
AnastasiyaMAV <mihalukova112@mail.ru>
2021-11-29 14:29:30 +0200

    Добавление сборщика browserify

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide('.splide', {
+      type: 'loop',
+      width: '90%',
+      gap: 25,
+      arrows: 'slider',
+      perPage: 3,
+      focus: 'center',
+      slideFocus: true,
+      flickMaxPages: 3,
+      updateOnMove: true,
+      pagination: true,
+      padding: 0,
+      throttle: 300,
+      breakpoints: {
+        768: {
+          perPage: 1,
+          padding: 0,
+          arrows: false,
+        }
+      }
+    }).mount();
+  });
+  
