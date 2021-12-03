19875865d6c305e8f99471c8050ebcbd9c706caf
AnastasiyaMAV <mihalukova112@mail.ru>
2021-12-03 11:41:45 +0200

    Обновила верстку блока inspiration

--- scripts/index.js
+++ scripts/index.js
@@ -22,35 +22,47 @@ function showFavList() {
 }
 
 //----------Блок 50+ Beautiful rooms inspiration----------
-let arrayImg = [
-    "assets/images/inspiration/Rectangle25.png",
-    "assets/images/inspiration/22-10.jpeg",
-    "assets/images/inspiration/22-11.jpeg"
-] // массив картинок
+// let arrayImg = [
+//     "assets/images/inspiration/Rectangle25.png",
+//     "assets/images/inspiration/22-10.jpeg",
+//     "assets/images/inspiration/22-11.jpeg"
+// ] // массив картинок
 
-let counter = -1; // Счетчик, указывающий на текущую картинки
+// let counter = -1; // Счетчик, указывающий на текущую картинки
 
-function changeImgNext() { //функция кнопки 
-    let img = document.getElementById("room"); // ищем картинку
-    if (counter < arrayImg.length - 1) //
-        counter++;
-    else {
-        counter = 0; //
-    }
-    img.src = arrayImg[counter]; // устанавливает картинку по индексу каунтер
-}
+// function changeImgNext() { //функция кнопки 
+//     let img = document.getElementById("room"); // ищем картинку
+//     if (counter < arrayImg.length - 1) //
+//         counter++;
+//     else {
+//         counter = 0; //
+//     }
+//     img.src = arrayImg[counter]; // устанавливает картинку по индексу каунтер
+// }
 
-function changeImgPrev() { //функция кнопки 
-    let img = document.getElementById("room"); // ищем картинку
-    if (counter > 0) //
-        counter--;
-    else {
-        counter = arrayImg.length - 1; //
-    }
-    img.src = arrayImg[counter]; // устанавливает картинку по индексу каунтер
-    
-}
+// function changeImgPrev() { //функция кнопки 
+//     let img = document.getElementById("room"); // ищем картинку
+//     if (counter > 0) //
+//         counter--;
+//     else {
+//         counter = arrayImg.length - 1; //
+//     }
+//     img.src = arrayImg[counter]; // устанавливает картинку по индексу каунтер
+// }
+new Swiper('.image-slider', {
+    loop: true,
 
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+
+    effect: 'fade',
+
+    fadeEffect: {
+        crossFade: true,
+    },
+});
 //----------Блок Tips & Tricks----------
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide('.splide', {
